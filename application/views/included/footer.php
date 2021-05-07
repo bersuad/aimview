045575aab95d2c8cb4fdde2039c36b5cafeb70c7
bersuad <bersuadane@gmail.com>
2021-05-07 20:03:35 +0300

    interface

---
+++ application/views/included/footer.php
@@ -0,0 +1,134 @@
+<footer id="wm-footer" class="wm-footer-one">
+
+    
+    <div class="wm-footer-widget">
+        <div class="container">
+            <div class="row">
+                <div class="tab-pane" id="profile">
+                    <!-- <span class="wm-contact-title">Contact Info</span> -->
+                    <div class="wm-contact-service">
+                        <ul class="row">
+                            <li class="col-md-4">
+                                <span class="wm-service-icon"><i class="wmicon-pin"></i></span>
+                                <h5 class="wm-color">Address</h5>
+                                <p>MEXICO K KARE BUILDING 8TH FLOOR, OFFICE NUMBER 812/1, ADDIS ABABA, ETHIOPIA</p>
+                            </li>
+                            <li class="col-md-4">
+                                <span class="wm-service-icon"><i class="wmicon-phone"></i></span>
+                                <h5 class="wm-color">Phone & Fax</h5>
+                                <p>+251 924 77 02 52 <br> +251 922 02 41 97</p>
+                            </li>
+                            <li class="col-md-4">
+                                <span class="wm-service-icon"><i class="wmicon-letter"></i></span>
+                                <h5 class="wm-color">E-mail</h5>
+                                <p><a href="mailto:contact@aimview.et">contact@aimview.et</a> <a href="mailto:info@aimview.et">info@aimview.et</a></p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div align="center">
+                        <ul class="contact-social-icon">
+                            <li><a href="#"><i class="wm-color wmicon-social5"></i> Facebook</a></li>
+                            <li><a href="#"><i class="wm-color wmicon-social4"></i> Twitter</a></li>
+                            <li><a href="#"><i class="wm-color wmicon-social3"></i> Linkedin</a></li>
+                            <li><a href="#"><i class="wm-color wmicon-social6"></i> Telegram</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+            
+    <div class="modal fade" id="ModalLogin" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-body">
+            
+            <div class="wm-modallogin-form wm-login-popup">
+                <span class="wm-color">Login to Your Account</span>
+                <form>
+                    <ul>
+                        <li> <input type="text" value="Your Username" onblur="if(this.value == '') { this.value ='Your Username'; }" onfocus="if(this.value =='Your Username') { this.value = ''; }"> </li>
+                        <li> <input type="password" value="password" onblur="if(this.value == '') { this.value ='password'; }" onfocus="if(this.value =='password') { this.value = ''; }"> </li>
+                        <li> <a href="#" class="wm-forgot-btn">Forgot Password?</a> </li>
+                        <li> <input type="submit" value="Sign In"> </li>
+                    </ul>
+                </form>
+                <span class="wm-color">or try our socials</span>
+                <ul class="wm-login-social-media">
+                    <li><a href="#"><i class="wmicon-social5"></i> Facebook</a></li>
+                    <li class="wm-twitter-color"><a href="#"><i class="wmicon-social4"></i> twitter</a></li>
+                    <li class="wm-googleplus-color"><a href="#"><i class="fa fa-google-plus-square"></i> Google+</a></li>
+                </ul>
+                <p>Not a member yet? <a href="#">Sign-up Now!</a></p>
+            </div>
+            <div class="wm-modallogin-form wm-register-popup">
+                <span class="wm-color">create Your Account today</span>
+                <form>
+                    <ul>
+                        <li> <input type="text" value="Your Username" onblur="if(this.value == '') { this.value ='Your Username'; }" onfocus="if(this.value =='Your Username') { this.value = ''; }"> </li>
+                        <li> <input type="text" value="Your E-mail" onblur="if(this.value == '') { this.value ='Your E-mail'; }" onfocus="if(this.value =='Your E-mail') { this.value = ''; }"> </li>
+                        <li> <input type="password" value="password" onblur="if(this.value == '') { this.value ='password'; }" onfocus="if(this.value =='password') { this.value = ''; }"> </li>
+                        <li> <input type="text" value="Confirm Password" onblur="if(this.value == '') { this.value ='Confirm Password'; }" onfocus="if(this.value =='Confirm Password') { this.value = ''; }"> </li>
+                        <li> <input type="submit" value="Create Account"> </li>
+                    </ul>
+                </form>
+                <span class="wm-color">or signup with your socials:</span>
+                <ul class="wm-login-social-media">
+                    <li><a href="#"><i class="wmicon-social5"></i> Facebook</a></li>
+                    <li class="wm-twitter-color"><a href="#"><i class="wmicon-social4"></i> twitter</a></li>
+                    <li class="wm-googleplus-color"><a href="#"><i class="fa fa-google-plus-square"></i> Google+</a></li>
+                </ul>
+                <p>Already a member? <a href="#">Sign-in Here!</a></p>
+            </div>
+
+          </div>
+        </div>
+      <div class="clearfix"></div>
+      </div>
+    </div>
+    <!-- ModalLogin Box -->
+
+    <!-- ModalSearch Box -->
+    <div class="modal fade" id="ModalSearch" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+            <div class="wm-modallogin-form">
+                <span class="wm-color">ለሶስት ቀናት የነጻ አገልግሎት ፣ በቀን 2ብር ብቻ ።
+                    <br>Free for 3 days, after it would cost 2 Birr/day.</span>
+                <form>
+                    <ul>
+                        <li> <input type="text" placeholder="09xxxxxxxx"> </li>
+                        <li> <input type="submit" value="Register"> </li>
+                    </ul>
+                </form>
+            </div>
+
+          </div>
+        </div>
+      <div class="clearfix"></div>
+      </div>
+    </div>
+    <!-- ModalSearch Box -->
+
+	<!-- jQuery (necessary for JavaScript plugins) -->
+	<script type="text/javascript" src="<?php echo base_url() ?>assets/script/jquery.js"></script>
+	<script type="text/javascript" src="<?php echo base_url() ?>assets/script/modernizr.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets/script/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets/script/jquery.prettyphoto.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets/script/jquery.countdown.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets/script/fitvideo.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets/script/skills.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets/script/slick.slider.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets/script/waypoints-min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets/build/mediaelement-and-player.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets/script/isotope.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets/script/jquery.nicescroll.min.js"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets/script/functions.js"></script>
+
+  </body>
+
+<!--  15:20  -->
+</html>
---
+++ application/views/included/header.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AIM View - ኤም ቪው</title>
+    <link rel="shortcut icon" href="<?php echo base_url() ?>assets/images/logo/vertical_logo.png">
+    <link href="<?php echo  base_url() ?>assets/css/bootstrap.css" rel="stylesheet">
+    <link href="<?php echo  base_url() ?>assets/css/font-awesome.css" rel="stylesheet">
+    <link href="<?php echo  base_url() ?>assets/css/flaticon.css" rel="stylesheet">
+    <link href="<?php echo  base_url() ?>assets/css/slick-slider.css" rel="stylesheet">
+    <link href="<?php echo  base_url() ?>assets/css/prettyphoto.css" rel="stylesheet">
+    <link href="<?php echo  base_url() ?>assets/build/mediaelementplayer.css" rel="stylesheet">
+    <link href="<?php echo  base_url() ?>assets/style.css" rel="stylesheet">
+    <link href="<?php echo  base_url() ?>assets/css/color.css" rel="stylesheet">
+    <link href="<?php echo  base_url() ?>assets/css/color-two.css" rel="stylesheet">
+    <link href="<?php echo  base_url() ?>assets/css/color-three.css" rel="stylesheet">
+    <link href="<?php echo  base_url() ?>assets/css/color-four.css" rel="stylesheet">
+    <link href="<?php echo  base_url() ?>assets/css/responsive.css" rel="stylesheet">
+  </head>
+  <body>
+	
+    <!--// Main Wrapper \\-->
+    <div class="wm-main-wrapper">
+        
+        <!--// Header \\-->
+		<header id="wm-header" class="wm-header-one">
+
+            <!--// TopStrip \\-->
+			<div class="wm-topstrip">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="wm-language"> <ul> <li><a href="#">English</a></li> <li><a href="#">አማርኛ</a></li> </ul> </div>
+                            <ul class="wm-stripinfo">
+                                <li><i class="wmicon-location"></i>Mexico K Kare Building 8th floor, Office number 812/1, Addis Ababa, Ethiopia</li>
+                                <li><i class="wmicon-technology4"></i>+251 924 77 02 52, +251 922 02 41 97</li>
+                                <li><i class="wmicon-clock2"></i> Mon - fri: 8:30am - 5:00pm</li>
+                            </ul>
+                            <!-- <ul class="wm-adminuser-section">
+                                <li>
+                                    <a href="#" data-toggle="modal" data-target="#ModalLogin">login</a>
+                                </li>
+                                <li>
+                                    <a href="#">Contact</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="wm-search-btn" data-toggle="modal" data-target="#ModalSearch"><i class="wmicon-search"></i></a>
+                                </li>
+                            </ul> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--// TopStrip \\-->
+
+            <!--// MainHeader \\-->
+            <div class="wm-main-header">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4"><a href="<?php echo base_url('') ?>" class="wm-logo"><img src="<?php echo base_url() ?>assets/images/logo/horizontal_logo.png" alt="" class="main_logo"></a></div>
+                        <div class="col-md-8">
+                            <!--// Navigation \\-->
+                            <nav class="navbar navbar-default">
+                                <div class="navbar-header">
+                                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="true">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                  </button>
+                                </div>
+                                <div class="collapse navbar-collapse" id="navbar-collapse-1">
+                                  <ul class="nav navbar-nav">
+                                    <li class="active"><a href="#">Home</a>
+                                    </li>
+                                    <li><a href="#">Why Aim View</a></li>
+                                    <li><a href="#">Do you have ideas?</a>
+                                        <ul class="wm-dropdown-menu">
+                                            <li><a href="#">Teacher</a></li>
+                                            <li><a href="#">Student</a></li>
+                                            <li><a href="#">Parent</a></li>
+                                            <li><a href="#">Community</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="wm-megamenu-li"><a href="#">Contact</a></li>
+                                  </ul>
+                                </div>
+                            </nav>
+                            <!--// Navigation \\-->
+                            <a href="#" class="wm-header-btn" data-toggle="modal" data-target="#ModalSearch">Register Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--// MainHeader \\-->
+
+		</header>
